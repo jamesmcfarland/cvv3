d084bdedb3b1da1d39454e2ce1e8adfc48396da5
James McFarland <24841376+jamesmcfarland@users.noreply.github.com>
2022-06-02 03:36:02 +0100

    Added head

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Copyright from "../components/Copyright";
 import Landing from "../components/landing/Landing";
 import LetsTalk from "../components/LetsTalk";
@@ -6,6 +7,9 @@ import MainContent from "../components/MainContent";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>James McFarland</title>
+      </Head>
       <Landing maxFadeInDelay={2} />
       <MainContent />
       <LetsTalk />
